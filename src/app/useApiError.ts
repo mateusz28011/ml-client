54090646c00169fe605c1ee880bf67b94f459f23
mateusz28011 <mathew28011@gmail.com>
2021-11-13 18:09:37 +0100

    Creating custom hook to handle api errors

---
+++ src/app/useApiError.ts
@@ -0,0 +1,73 @@
+import { SerializedError } from '@reduxjs/toolkit';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
+import { useEffect, useState } from 'react';
+import { ApiError } from './services/split';
+// import { RegisterRequest } from './services/split/auth';
+// import { keys } from 'ts-transformer-keys';
+// Partial<RequestData>
+type ApiErrorReturn<T> = {
+  [Key in keyof T | 'mainError']?: { error: any; isError: boolean };
+};
+
+const hasNameError = (
+  error: FetchBaseQueryError | ApiError | SerializedError | undefined,
+  name: string
+): [any, boolean] =>
+  error !== undefined
+    ? 'data' in error
+      ? name in error.data
+        ? [error.data[name], true]
+        : [undefined, false]
+      : [undefined, false]
+    : [undefined, false];
+
+const checkMainError = (
+  error: FetchBaseQueryError | ApiError | SerializedError | undefined
+) => {
+  if (
+    error !== undefined &&
+    'status' in error &&
+    typeof error?.status === 'string'
+  ) {
+    return { error: 'Server error', isError: true };
+  } else {
+    return { error: undefined, isError: false };
+  }
+};
+
+const checkErrors = <T, Key extends keyof T>(
+  error: FetchBaseQueryError | ApiError | SerializedError | undefined,
+  keys: Array<Key>
+): ApiErrorReturn<T> => {
+  return keys.reduce(
+    (a, v) => {
+      const [errorData, isNameError] = hasNameError(error, v as string);
+      return {
+        ...a,
+        [v]: {
+          error: errorData,
+          isError: isNameError,
+        },
+      };
+    },
+    { mainError: checkMainError(error) }
+  );
+};
+
+const useApiError = <RequestData, Key extends keyof RequestData>(
+  error: FetchBaseQueryError | ApiError | SerializedError | undefined,
+  keys: Array<Key>
+): ApiErrorReturn<RequestData> => {
+  const [keysOfRequestData] = useState(keys);
+  const [errors, setErrors] = useState<ApiErrorReturn<RequestData>>({});
+
+  useEffect(() => {
+    setErrors(
+      checkErrors<RequestData, keyof RequestData>(error, keysOfRequestData)
+    );
+  }, [error, keysOfRequestData]);
+
+  return errors;
+};
+
+export default useApiError;
